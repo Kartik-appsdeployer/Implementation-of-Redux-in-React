937fc0cb79943350c05fd4d71a7af18499003b9a
KartikKulshreshtha <76987281+KartikKulshreshtha@users.noreply.github.com>
2023-02-13 15:38:43 +0530

    Added the redux-persists

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import todoReducer from './reducer';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, todoReducer);
 
 
 export const store = configureStore({
-  reducer: { todo: todoReducer },
+  reducer: { todo: persistedReducer,
+             middleware: [thunk] },
 })
+
+export const persistor = persistStore(store)
